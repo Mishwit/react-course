83e2f8bdbc6bc9915942609706fb0f6a53042f51
Mishwit <mgavrilovlife@gmail.com>
2023-07-04 17:17:07 +0200

    start react

---
+++ cars-catalog/src/components/screens/home/cars.data.js
@@ -0,0 +1,23 @@
+export const cars = [
+  {
+    Id: 1,
+    name: "Toyota Camry",
+    price: 24970,
+    image:
+      "https://www.cnet.com/a/img/resize/4476006d1312ec379ee616af256b7596321b3742/hub/2021/08/20/257caf0a-f3a2-45db-9c70-d6ed50a85e6f/2021-toyota-camry-trd-ogi-1.jpg?auto=webp&fit=crop&height=900&width=1200",
+  },
+  {
+    id: 2,
+    name: "Honda Civic",
+    price: 22545,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6d/2017_Honda_Civic_SR_VTEC_1.0_Front.jpg",
+  },
+  {
+    id: 3,
+    name: "Honda Civic 2",
+    price: 15545,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6d/2017_Honda_Civic_SR_VTEC_1.0_Front.jpg",
+  },
+];
